85c911c0cb41ecbea7e710370203ba16c9bb738f
Amruta <amruta9.kulkarni@gmail.com>
2019-01-05 23:21:01 +0000

    changes

---
+++ app/p2p-server.js
@@ -0,0 +1,46 @@
+const websocket = require('ws');
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+
+class P2pServer {
+    constructor(blockchain){
+        this.blockchain = blockchain;
+        this.sockets = []; 
+    }
+
+    listen(){
+        const server = new websocket.Server({port : P2P_PORT});
+        server.on('connection',socket => this.connectSocket(socket));
+
+        this.connectToPeers();
+        console.log(`listening for p2p connections on:  ${P2P_PORT}`);
+    }
+
+    connectToPeers(){
+
+        peers.forEach(peer =>{
+            const socket = new websocket(peer);
+            socket.on('open',()=> this.connectSocket(socket));
+        });
+
+    }
+
+    connectSocket(socket){
+        this.sockets.push(socket);
+        console.log('socket connected');
+
+        this.messageHandler(socket);
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    messageHandler(socket){
+        socket.on('message', message=>{
+            const data = JSON.parse(message);
+            console.log('data is : ', data);
+        });
+    }
+}
+
+module.exports = P2pServer;
